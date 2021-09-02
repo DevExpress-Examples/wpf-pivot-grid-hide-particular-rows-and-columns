88021206e0a3b06dcd716307897e8e73cff78fb2
DevExpressExampleBot <fakeEmail>
2021-09-02 16:04:00 +0300

    autogenerate VB

--- VB/Data.vb
+++ VB/Data.vb
@@ -1,9 +1,8 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
+﻿Imports System.Data
 Imports DevExpress.Xpf.PivotGrid
 
 Namespace DXPivotGrid_HidingColumnsAndRows
-	Public NotInheritable Class PivotHelper
+	Public Module PivotHelper
 		Public Const Employee As String = "Employee"
 		Public Const Widget As String = "Widget"
 		Public Const Month As String = "Month"
@@ -18,9 +17,7 @@ Namespace DXPivotGrid_HidingColumnsAndRows
 		Public Const WidgetB As String = "Widget B"
 		Public Const WidgetC As String = "Widget C"
 
-		Private Sub New()
-		End Sub
-		Public Shared Sub FillPivot(ByVal pivot As PivotGridControl)
+		Public Sub FillPivot(ByVal pivot As PivotGridControl)
 			pivot.Fields.Add(Employee, FieldArea.RowArea)
 			pivot.Fields.Add(Widget, FieldArea.RowArea)
 			pivot.Fields.Add(Month, FieldArea.ColumnArea).AreaIndex = 0
@@ -37,7 +34,7 @@ Namespace DXPivotGrid_HidingColumnsAndRows
 			pivot.DataFieldArea = DataFieldArea.ColumnArea
 			pivot.DataFieldAreaIndex = 1
 		End Sub
-		Private Shared Function GetAllowedArea(ByVal area As FieldArea) As FieldAllowedAreas
+		Private Function GetAllowedArea(ByVal area As FieldArea) As FieldAllowedAreas
 			Select Case area
 				Case FieldArea.ColumnArea
 					Return FieldAllowedAreas.ColumnArea
@@ -51,7 +48,7 @@ Namespace DXPivotGrid_HidingColumnsAndRows
 					Return FieldAllowedAreas.All
 			End Select
 		End Function
-		Public Shared Function GetDataTable() As DataTable
+		Public Function GetDataTable() As DataTable
 			Dim table As New DataTable()
 			table.Columns.Add(Employee, GetType(String))
 			table.Columns.Add(Widget, GetType(String))
@@ -74,5 +71,5 @@ Namespace DXPivotGrid_HidingColumnsAndRows
 			table.Rows.Add(EmployeeB, WidgetC, 7, 40.0, 38.3, 7, 1)
 			Return table
 		End Function
-	End Class
+	End Module
 End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports Microsoft.VisualBasic
+﻿Imports System
 Imports System.Globalization
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
@@ -6,6 +6,7 @@ Imports DevExpress.Xpf.PivotGrid
 Namespace DXPivotGrid_HidingColumnsAndRows
 	Partial Public Class Window1
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
 			AddHandler pivotGrid.CustomFieldValueCells, AddressOf pivotGrid_CustomFieldValueCells
@@ -18,36 +19,34 @@ Namespace DXPivotGrid_HidingColumnsAndRows
 
 		' Handles the CustomFieldValueCells event to remove
 		' specific rows.
-        Private Sub pivotGrid_CustomFieldValueCells(ByVal sender As Object, _
-                                                    ByVal e As PivotCustomFieldValueCellsEventArgs)
-            If pivotGrid.DataSource Is Nothing Then
-                Return
-            End If
-            If rbDefault.IsChecked = True Then
-                Return
-            End If
+		Private Sub pivotGrid_CustomFieldValueCells(ByVal sender As Object, ByVal e As PivotCustomFieldValueCellsEventArgs)
+			If pivotGrid.DataSource Is Nothing Then
+				Return
+			End If
+			If rbDefault.IsChecked = True Then
+				Return
+			End If
 
-            ' Iterates through all row headers.
-            For i As Integer = e.GetCellCount(False) - 1 To 0 Step -1
-                Dim cell As FieldValueCell = e.GetCell(False, i)
-                If cell Is Nothing Then Continue For
+			' Iterates through all row headers.
+			For i As Integer = e.GetCellCount(False) - 1 To 0 Step -1
+				Dim cell As FieldValueCell = e.GetCell(False, i)
+				If cell Is Nothing Then
+					Continue For
+				End If
 
-                ' If the current header corresponds to the "Employee B"
-                ' field value, and is not the Total Row header,
-                ' it is removed with all corresponding rows.
-                If Object.Equals(cell.Value, "Employee B") AndAlso _
-                        cell.ValueType <> FieldValueType.Total Then
-                    e.Remove(cell)
-                End If
-            Next i
-        End Sub
-        Private Sub pivotGrid_FieldValueDisplayText(ByVal sender As Object, _
-                                                    ByVal e As PivotFieldDisplayTextEventArgs)
-            If Object.Equals(e.Field, pivotGrid.Fields(PivotHelper.Month)) Then
-                e.DisplayText = _
-                    CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(CInt(Fix(e.Value)))
-            End If
-        End Sub
+				' If the current header corresponds to the "Employee B"
+				' field value, and is not the Total Row header,
+				' it is removed with all corresponding rows.
+				If Object.Equals(cell.Value, "Employee B") AndAlso cell.ValueType <> FieldValueType.Total Then
+					e.Remove(cell)
+				End If
+			Next i
+		End Sub
+		Private Sub pivotGrid_FieldValueDisplayText(ByVal sender As Object, ByVal e As PivotFieldDisplayTextEventArgs)
+			If e.Field = pivotGrid.Fields(PivotHelper.Month) Then
+				e.DisplayText = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(CInt(Math.Truncate(e.Value)))
+			End If
+		End Sub
 		Private Sub rbDefault_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
 			pivotGrid.LayoutChanged()
 		End Sub
