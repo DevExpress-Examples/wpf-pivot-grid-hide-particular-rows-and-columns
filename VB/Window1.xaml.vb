5a9e0d703fcd86a10d2f33b952f6e794a977012e
DevExpressExampleBot <fakeEmail>
2023-12-05 18:56:44 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,55 +1,47 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Globalization
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 
 Namespace DXPivotGrid_HidingColumnsAndRows
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			AddHandler pivotGrid.CustomFieldValueCells, AddressOf pivotGrid_CustomFieldValueCells
-		End Sub
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			PivotHelper.FillPivot(pivotGrid)
-			pivotGrid.DataSource = PivotHelper.GetDataTable()
-			pivotGrid.BestFit()
-		End Sub
 
-		' Handles the CustomFieldValueCells event to remove
-		' specific rows.
-        Private Sub pivotGrid_CustomFieldValueCells(ByVal sender As Object, _
-                                                    ByVal e As PivotCustomFieldValueCellsEventArgs)
-            If pivotGrid.DataSource Is Nothing Then
-                Return
-            End If
-            If rbDefault.IsChecked = True Then
-                Return
-            End If
+    Public Partial Class Window1
+        Inherits Window
 
+        Public Sub New()
+            Me.InitializeComponent()
+            AddHandler Me.pivotGrid.CustomFieldValueCells, New PivotCustomFieldValueCellsEventHandler(AddressOf pivotGrid_CustomFieldValueCells)
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            PivotHelper.FillPivot(Me.pivotGrid)
+            Me.pivotGrid.DataSource = GetDataTable()
+            Me.pivotGrid.BestFit()
+        End Sub
+
+        ' Handles the CustomFieldValueCells event to remove
+        ' specific rows.
+        Private Sub pivotGrid_CustomFieldValueCells(ByVal sender As Object, ByVal e As PivotCustomFieldValueCellsEventArgs)
+            If Me.pivotGrid.DataSource Is Nothing Then Return
+            If Me.rbDefault.IsChecked = True Then Return
             ' Iterates through all row headers.
             For i As Integer = e.GetCellCount(False) - 1 To 0 Step -1
                 Dim cell As FieldValueCell = e.GetCell(False, i)
                 If cell Is Nothing Then Continue For
-
                 ' If the current header corresponds to the "Employee B"
                 ' field value, and is not the Total Row header,
                 ' it is removed with all corresponding rows.
-                If Object.Equals(cell.Value, "Employee B") AndAlso _
-                        cell.ValueType <> FieldValueType.Total Then
-                    e.Remove(cell)
-                End If
-            Next i
+                If Equals(cell.Value, "Employee B") AndAlso cell.ValueType <> FieldValueType.Total Then e.Remove(cell)
+            Next
         End Sub
-        Private Sub pivotGrid_FieldValueDisplayText(ByVal sender As Object, _
-                                                    ByVal e As PivotFieldDisplayTextEventArgs)
-            If Object.Equals(e.Field, pivotGrid.Fields(PivotHelper.Month)) Then
-                e.DisplayText = _
-                    CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(CInt(Fix(e.Value)))
+
+        Private Sub pivotGrid_FieldValueDisplayText(ByVal sender As Object, ByVal e As PivotFieldDisplayTextEventArgs)
+            If e.Field Is Me.pivotGrid.Fields(Month) Then
+                e.DisplayText = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(CInt(e.Value))
             End If
         End Sub
-		Private Sub rbDefault_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			pivotGrid.LayoutChanged()
-		End Sub
-	End Class
+
+        Private Sub rbDefault_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.pivotGrid.LayoutChanged()
+        End Sub
+    End Class
 End Namespace
